e96c0260b398a8889fd3d8d4f4d7ed0f09aa0da1
asrevelo23 <32751263+asrevelo23@users.noreply.github.com>
2019-03-13 12:10:51 -0500

    Update color.js

--- skills/color.js
+++ skills/color.js
@@ -6,7 +6,7 @@ module.exports = function (controller) {
     controller.hears([/^start/], 'direct_message,direct_mention', function (bot, message) {
 
         bot.startConversation(message, function (err, convo) {
-            convo.say('This is a Botkit conversation sample.');
+            convo.say('I am a botkit that will help your with school');
 
             convo.ask('What homework do you have due?', function (response, convo) {
                 convo.say("I can remind you about your '" + response.text + "' homework! Please type **help** to see al my functions.");
