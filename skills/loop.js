b0a74b626ba7f0b67e3d7244d551fca3c411f340
asrevelo23 <32751263+asrevelo23@users.noreply.github.com>
2019-03-13 12:21:27 -0500

    Update loop.js

--- skills/loop.js
+++ skills/loop.js
@@ -6,7 +6,7 @@
 
 module.exports = function (controller) {
 
-    controller.hears([/^weather/], 'direct_message,direct_mention', function (bot, message) {
+    controller.hears([/^resources/], 'direct_message,direct_mention', function (bot, message) {
 
         bot.startConversation(message, function (err, convo) {
 
