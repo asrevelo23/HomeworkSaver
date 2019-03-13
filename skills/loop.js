44bd55bbd4476166acaabfe6a3035fa2ca4ef1d2
asrevelo23 <32751263+asrevelo23@users.noreply.github.com>
2019-03-13 12:09:17 -0500

    Update loop.js

--- skills/loop.js
+++ skills/loop.js
@@ -10,37 +10,31 @@ module.exports = function (controller) {
 
         bot.startConversation(message, function (err, convo) {
 
-            var question = "Here are your most important cities:";
-            question += "<br/> `1)` Austin, Tx (**Austin**)";
-            question += "<br/> `2)` Houston, Tx (**Houston**)";
-            question += "<br/> `3)` San Salvador (**Salvador**)";
-            question += "<br/> `4)` La Libertad (**Libertad**)";
-            question += "\n\nWhat wearher do you want to check?<br/>_(type a number, a **bold keyword** or `stop`)_";
+            var question = "Here are a few school tips:";
+            question += "<br/> `1)` How to handle stress (**stress**)";
+            question += "<br/> `2)` How to become a good test-taker (**test**)";
+            question += "<br/> `3)` check Upcoming Events at UT (**events**)";
+            question += "\n\nWhat do you want to check?<br/>_(type a number, a **bold keyword** or `stop`)_";
             convo.ask(question, [
                 {
-                    pattern: "1|Austin|austin",
+                    pattern: "1|stress|Stress",
                     callback: function (response, convo) {
                         convo.gotoThread('menu_1');
                     },
                 }
                 , {
-                    pattern: "2|houston|Houston",
+                    pattern: "2|test|taker|test-taker",
                     callback: function (response, convo) {
                         convo.gotoThread('menu_2');
                     },
                 }
                 , {
-                    pattern: "3|San Salvador|Salvador|salvador",
+                    pattern: "3|event|express",
                     callback: function (response, convo) {
                         convo.gotoThread('menu_3');
                     },
                 }
-                , {
-                    pattern: "4|La Libertad|Libertad|libertad",
-                    callback: function (response, convo) {
-                        convo.gotoThread('menu_4');
-                    },
-                }
+                
                 , {
                     pattern: "cancel|stop",
                     callback: function (response, convo) {
@@ -57,27 +51,23 @@ module.exports = function (controller) {
 
             // Menu option 1)
             convo.addMessage({
-                text: "Lets see how Austin is doing today: now [discover Austin's weather](https://weather.com/weather/today/l/USTX0057:1:US). Be safe!",
+                text: "Sorry to hear that you are stressed: now [discover how to handle it](https://www.learnpsychology.org/student-stress-anxiety-guide/) online, and keep working hard...",
                 action: 'default'
             }, 'menu_1');
 
             // Menu option 2)
             convo.addMessage({
-                text: "Lets see how HTown is doing today: now [discover Houston's weather](https://weather.com/weather/today/l/USTX0617:1:US). Be safe!",
+                text: "Good taske-takers follow this instructions. [the learnings tracks](https://www.floridacareercollege.edu/blog/study-tips-to-become-a-better-test-taker) and become the best in your class!",
                 action: 'default'
             }, 'menu_2');
 
             // Menu option 3)
             convo.addMessage({
-                text: "Lets see how San Salvador is doing today: now [discover San Salvador's weather](https://weather.com/weather/today/l/ESXX0003:1:ES). Be safe!",
+                text: "Nothing's like meeting in person at a conference, training or a hackathon. Check the list of [UT events](https://calendar.utexas.edu/).",
                 action: 'default'
             }, 'menu_3');
             
-            // Menu option 4)
-            convo.addMessage({
-                text: "Lets see how San Miguel is doing today: now [discover San Miguel's weather](https://weather.com/weather/today/l/ESXX0007:1:ES). Be safe!",
-                action: 'default'
-            }, 'menu_4');
+          
 
             // Cancel
             convo.addMessage({
