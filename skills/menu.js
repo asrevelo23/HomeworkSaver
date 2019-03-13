c90e4ef406dd70aa5c14d9064cafb662fb93aa82
asrevelo23 <32751263+asrevelo23@users.noreply.github.com>
2019-03-13 04:46:45 -0500

    Update menu.js

--- skills/menu.js
+++ skills/menu.js
@@ -14,38 +14,34 @@ module.exports = function (controller) {
 
         bot.startConversation(message, function (err, convo) {
 
-            var question = "Here are a few proposed DevNet activities:";
-            question += "<br/> `1)` join a Community Of Interest (**communities**)";
-            question += "<br/> `2)` take a Learning Lab (**labs**)";
-            question += "<br/> `3)` check Upcoming Events (**events**)";
-            question += "\n\nWhat do you want to do ?<br/>_(type a number, a **bold keyword** or `cancel`)_";
+             var question = "Here are a few school tips:";
+            question += "<br/> `1)` How to handle stress (**stress**)";
+            question += "<br/> `2)` How to become a good test-taker (**test**)";
+            question += "<br/> `3)` check Upcoming Events at UT (**events**)";
+            question += "\n\nWhat do you want to check?<br/>_(type a number, a **bold keyword** or `stop`)_";
             convo.ask(question, [
                 {
-                    pattern: "1|community|communities",
+                    pattern: "1|stress|Stress",
                     callback: function (response, convo) {
-                        convo.say("Excellent choice: now [check the DevNet communities](https://developer.cisco.com/site/coi/) online, and pick your favorite...");
-                        convo.next();
+                        convo.gotoThread('menu_1');
                     },
                 }
                 , {
-                    pattern: "2|lab|track|learn",
+                    pattern: "2|test|taker|test-taker",
                     callback: function (response, convo) {
-                        convo.say("Learnings **labs** are step-by-step tutorials. They are grouped into **tracks** to help you on your rampup journey. Just browse through [the learnings tracks](https://learninglabs.cisco.com/login) and pick the labs that suits your learning appetite!");
-                        convo.next();
+                        convo.gotoThread('menu_2');
                     },
                 }
                 , {
                     pattern: "3|event|express",
                     callback: function (response, convo) {
-                        convo.say("Nothing's like meeting in person at a conference, training or a hackathon. Check the list of [DevNet events](https://developer.cisco.com/site/devnet/events-contests/events/) or ask the bot: invite `CiscoDevNet@sparkbot.io` to chat in a Webex Teams space.");
-                        convo.next();
+                        convo.gotoThread('menu_3');
                     },
                 }
                 , {
                     pattern: "cancel|stop",
                     callback: function (response, convo) {
-                        convo.say("Got it, cancelling...");
-                        convo.next();
+                        convo.gotoThread('action_cancel');
                     },
                 }
                 , {
@@ -56,11 +52,36 @@ module.exports = function (controller) {
                 }
             ]);
 
+            // Menu option 1)
+            convo.addMessage({
+                text: "Sorry to hear that you are stressed: now [discover how to handle it](https://www.learnpsychology.org/student-stress-anxiety-guide/) online, and keep working hard...",
+                action: 'default'
+            }, 'menu_1');
+
+            // Menu option 2)
+            convo.addMessage({
+                text: "Good taske-takers follow this instructions. [the learnings tracks](https://www.floridacareercollege.edu/blog/study-tips-to-become-a-better-test-taker) and become the best in your class!",
+                action: 'default'
+            }, 'menu_2');
+
+            // Menu option 3)
+            convo.addMessage({
+                text: "Nothing's like meeting in person at a conference, training or a hackathon. Check the list of [UT events](https://calendar.utexas.edu/).",
+                action: 'default'
+            }, 'menu_3');
+
+            // Cancel
+            convo.addMessage({
+                text: "Got it, cancelling...",
+                action: 'stop', // this marks the converation as unsuccessful
+            }, 'action_cancel');
+
             // Bad response
             convo.addMessage({
                 text: "Sorry, I did not understand.",
                 action: 'default',
             }, 'bad_response');
+
         });
     });
 };
